ca310fd8d8e54b42d2f2380605e79dd663457a82
Hamish Coleman <hamish@zot.org>
2024-02-28 13:38:03 +1100

    Add two more test packet types

--- scripts/test_integration_packets.sh
+++ scripts/test_integration_packets.sh
@@ -28,24 +28,41 @@ docmd "${BINDIR}"/apps/supernode \
     -Osupernode.macaddr=02:00:00:00:00:01 \
     start ci_sn1
 
-
+# Register a mac address with the supernode
 docmd "${BINDIR}"/scripts/test_packets \
-    --bind=7000 \
+    --bind 7000 \
     -s localhost:7001 \
     test_REGISTER_SUPER
 
+# Confirm that the registration is visible
 docmd "${TOPDIR}"/scripts/n3nctl -s ci_sn1 get_edges --raw |grep -v last_seen
 
-# Once we register, we can query for that registration
+# Once registered, we can query for that registration
 docmd "${TOPDIR}"/scripts/test_packets \
     --bind 7000 \
     -s localhost:7001 \
     test_QUERY_PEER
 
+# Just request a PONG packet
 docmd "${BINDIR}"/scripts/test_packets \
     --bind 7000 \
     -s localhost:7001 \
     test_QUERY_PEER_ping
+
+# Since we are bound to the same udp port as the registration, when we
+# send a register to that, the supernode forwards it to us - as we are
+# listening on that registered port, we get the reply
+docmd "${TOPDIR}"/scripts/test_packets \
+    --bind 7000 \
+    -s localhost:7001 \
+    test_REGISTER
+
+# Send a packet to the registered mac, expect it echoed back
+docmd "${TOPDIR}"/scripts/test_packets \
+    --bind 7000 \
+    -s localhost:7001 \
+    test_PACKET
+
 
 docmd "${BINDIR}"/scripts/test_packets \
     --bind 7000 \
