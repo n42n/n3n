30b832fa8876f12a34de62513768fd8ba92d7ede
Hamish Coleman <hamish@zot.org>
2024-11-19 22:06:41 +1100

    Fix TCP RX path after UDP mainloop conversion

--- src/mainloop.c
+++ src/mainloop.c
@@ -199,6 +199,12 @@ static int setup_select (fd_set *rd, fd_set *wr, struct n3n_runtime_data *eee) {
     FD_ZERO(wr);
     int max_sock = fdlist_read_fd_set(rd);
 
+    // HACK - until this mainloop supports v3tcp proto sockets
+    if((eee->conf.connect_tcp) && (eee->sock != -1)) {
+        FD_SET(eee->sock, rd);
+        max_sock = MAX(max_sock, eee->sock);
+    }
+
     max_sock = MAX(
         max_sock,
         slots_fdset(
