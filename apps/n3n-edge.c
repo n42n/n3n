18f7cf5e21b64147ef30ba714f115771a2bd17ed
Hamish Coleman <hamish@zot.org>
2026-03-14 13:50:43 +1100

    Rename the check mode subcmd to match the action it performs

--- apps/n3n-edge.c
+++ apps/n3n-edge.c
@@ -348,7 +348,7 @@ static void cmd_test_benchmark (int argc, char **argv, void *_conf) {
     exit(0);
 }
 
-static void cmd_test_builtin (int argc, char **argv, void *_conf) {
+static void cmd_test_check (int argc, char **argv, void *_conf) {
     n2n_edge_conf_t *conf = (n2n_edge_conf_t *)_conf;
     int errors = benchmark_check_all(conf->test_output_format);
     if(errors) {
@@ -366,8 +366,8 @@ static void cmd_test_fakebench (int argc, char **argv, void *_conf) {
 }
 
 static void cmd_test_hashing (int argc, char **argv, void *conf) {
-    fprintf(stderr, "Deprecated: use `n3n-edge test builtin` instead\n");
-    cmd_test_builtin(argc, argv, conf);
+    fprintf(stderr, "Deprecated: use `n3n-edge test check` instead\n");
+    cmd_test_check(argc, argv, conf);
 }
 
 static void cmd_tools_keygen (int argc, char **argv, void *conf) {
@@ -561,16 +561,16 @@ static struct n3n_subcmd_def cmd_tools[] = {
 static struct n3n_subcmd_def cmd_test[] = {
     {
         .name = "benchmark",
-        .help = "run internal benchmarks",
+        .help = "run built-in tests and benchmark results",
         .type = n3n_subcmd_type_fn,
         .fn = &cmd_test_benchmark,
         .session_arg = true,
     },
     {
-        .name = "builtin",
-        .help = "run built-in tests",
+        .name = "check",
+        .help = "run built-in tests and check results",
         .type = n3n_subcmd_type_fn,
-        .fn = &cmd_test_builtin,
+        .fn = &cmd_test_check,
         .session_arg = true,
     },
     {
@@ -586,7 +586,7 @@ static struct n3n_subcmd_def cmd_test[] = {
     },
     {
         .name = "fakebench",
-        .help = "[name] - count test instructions (when perf is unavailable)",
+        .help = "[name] - run tests, counting instructions (when perf is unavailable)",
         .type = n3n_subcmd_type_fn,
         .fn = &cmd_test_fakebench,
         .session_arg = true,
